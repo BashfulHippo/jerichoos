9b388334cbac0cd07c9ab833c77b4cb4868b4240
BashfulHippo <136870655+BashfulHippo@users.noreply.github.com>
2025-10-08 21:00:00 -0700

    add serial output for debugging

---
+++ src/serial.rs
@@ -0,0 +1,42 @@
+//! Serial port driver for JerichoOS
+//!
+//! Provides serial output for debugging (QEMU can redirect to stdio)
+
+use lazy_static::lazy_static;
+use spin::Mutex;
+use uart_16550::SerialPort;
+
+lazy_static! {
+    /// Global serial port (COM1)
+    pub static ref SERIAL1: Mutex<SerialPort> = {
+        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1
+        .lock()
+        .write_fmt(args)
+        .expect("Printing to serial failed");
+}
+
+/// Print to serial port
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*))
+    };
+}
+
+/// Print to serial port with newline
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
+}
